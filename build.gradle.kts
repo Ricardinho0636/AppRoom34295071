96213d5b700ee31755c0cfaf5499941b859d9144
Ricardo Sbalqueiro <Ricardosbalqueiro@gmail.com>
2024-11-25 19:51:55 -0300

    AndroidManifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
 
 tasks.register("clean", Delete::class) {
