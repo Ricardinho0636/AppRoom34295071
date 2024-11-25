eb1bcfe391717a33cc74869d5d37afdf73c5b7d3
Ricardo Sbalqueiro <Ricardosbalqueiro@gmail.com>
2024-11-25 17:43:14 -0300

    Appcontainer,, Itemsrepository, offline

--- app/src/main/java/com/example/rgm34295071/data/ItemsRepository.kt
+++ app/src/main/java/com/example/rgm34295071/data/ItemsRepository.kt
@@ -16,4 +16,34 @@
 
 package com.example.rgm34295071.data
 
-interface ItemsRepository
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
+ */
+interface ItemsRepository {
+    /**
+     * Retrieve all the items from the the given data source.
+     */
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    /**
+     * Retrieve an item from the given data source that matches with the [id].
+     */
+    fun getItemStream(id: Int): Flow<Item?>
+
+    /**
+     * Insert item in the data source
+     */
+    suspend fun insertItem(item: Item)
+
+    /**
+     * Delete item from the data source
+     */
+    suspend fun deleteItem(item: Item)
+
+    /**
+     * Update item in the data source
+     */
+    suspend fun updateItem(item: Item)
+}
